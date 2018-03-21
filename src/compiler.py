6a096694fc88f1bac72d040ecbc1151795a7b406
nk17kumar <nk17kumar@gmail.com>
2018-03-22 00:41:28 +0530

    added compilation for all langages

---
+++ src/compiler.py
@@ -0,0 +1,52 @@
+import subprocess
+import sys
+import os
+
+class Operation:
+
+    path = os.path.abspath("../resources")
+
+    @staticmethod
+    def run_compiler(lang):
+        p = Operation.path
+        out = open(p+"/out.txt","w")
+        err = open(p+"/err.txt","r+")
+        fin = open(p +"/in.txt","r")
+        if lang == "python":
+            path1 = p + "/code.py"
+            subprocess.call(["python",path1],stdout=out,stderr=err,stdin=fin)
+        if lang == "c++":
+            p1 = p + "/code"
+            path1 = p + "/code.py"
+            sr = subprocess.call(["g++",path1,"-o",p1],stdout=out,stderr=err,stdin=fin)
+        if lang == "java":
+            path1 = p + "/code.java"
+            sr = subprocess.call(["javac",path],stdout=out,stderr=err,stdin=fin)
+        if lang == "c":
+            path1 = p + "/code.c"
+            sr = subprocess.call(["g++",path,"-o",p1],stdout=out,stderr=err,stdin=fin)
+
+        if(lang == "java"):
+            sr = subprocess.call(["java",p+"/code"])
+
+        compiling_err = err.read()
+
+        if compiling_err == "":
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_ac():
+        p = Operation.path
+        f1 = open(p+"/out.txt","r")
+        f2 = open(p+"/output.txt","r")
+        s1 = f1.read().strip()
+        s2 = f2.read().strip()
+        if(s1 == s2):
+            return True
+        else:
+            return False
+
+print Operation.run_compiler("python")
+print Operation.check_ac()
