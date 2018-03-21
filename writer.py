84a5ef9207b519fa1b38ac29b5a45aa19b2f32e6
nk17kumar <nk17kumar@gmail.com>
2018-03-21 22:25:00 +0530

    added code writer class

---
+++ writer.py
@@ -0,0 +1,17 @@
+class writer:
+
+    @staticmethod
+    def write_code(lang,code):
+        ext = ""
+        if lang == "python":
+            ext = "py"
+        if lang == "c++":
+            ext = "cpp"
+        if lang == "java":
+            ext = "java"
+        if lang == "c":
+            ext = "c"
+        filename = "code." + ext
+        fin = open(filename,"w")
+        fin.write(code)
+        fin.close()
