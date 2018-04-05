fa8475a81e94782b2c0b4278f523b5e36308ba92
csevirus <ketansharma144@gmail.com>
2018-04-05 23:56:27 +0530

    install file updated

--- src/install.py
+++ src/install.py
@@ -1,16 +1,14 @@
 import subprocess
 import os
-from main import project_path
 
-path = os.path.abspath("..")
-# project_path.set_path(path+"/src")
 sr = subprocess.call(["sudo", "apt-get", "install", "python-pip"])
 sr = subprocess.call(["sudo", "apt-get", "install", "default-jdk"])
 sr = subprocess.call(["sudo", "pip", "install", "getch"])
 sr = subprocess.call(["sudo", "pip", "install", "gspread"])
 sr = subprocess.call(["sudo", "pip", "install", "PyOpenSSL"])
-sr = subprocess.call(["sudo", "pip", "install","--upgrade" "oauth2client"])
-code = "\"[Desktop Entry]"+"\n"+"Version=1.0"+ "\n" +"Name=Blind Code"+"\n" +"Type=Application"+"\n" +"Terminal=true"+ "\n"+"Exec= gksu python "+ path + "/src/main.py"+ "\n" + "Icon=" + path + "/resources/icon.jpeg\""
+sr = subprocess.call(["sudo", "pip", "install","--upgrade", "oauth2client"])
+path = os.path.abspath("..")
 os.chdir("/usr/share/applications")
-sr = subprocess.call(["sudo", "touch", "blindcode.desktop"])
-sr = subprocess.call(["sudo", "echo", code , ">>" , "blindcode.desktop"])
+file = open("blindcode.desktop","w")
+code = "[Desktop Entry]"+"\n"+"Version=1.0"+ "\n" +"Name=Blind Code"+"\n" +"Type=Application"+"\n" +"Terminal=true"+ "\n"+"Exec= python "+ path + "/src/main.py"+ "\n" + "Icon=" + path + "/resources/icon.jpeg"
+file.write(code)
